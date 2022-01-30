b2b02c508622526ec71be5733f39c774ae702f06
ZnoGouDj <znogoud3@gmail.com>
2022-01-30 18:17:47 +0300

    Bug fix

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -27,7 +27,7 @@ const RightArrow = () => {
 const ImageScrollbar = ({ data }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }}>
     {data.map(item => (
-      <Box width="910px" itemId={item.id} overflow="hidden" padding="1">
+      <Box width="910px" key={item.id} overflow="hidden" padding="1">
         <Image
           alt="property"
           placeholder="blur"
